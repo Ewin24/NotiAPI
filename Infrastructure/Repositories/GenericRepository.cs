6a44cde953693e1863fae6f2857ad980a5e615d5
Ewin24 <edwintrigos24@gmail.com>
2023-10-18 11:13:30 -0500

    Feat: pruebas de los endpoints

--- Infrastructure/Repositories/GenericRepository.cs
+++ Infrastructure/Repositories/GenericRepository.cs
@@ -6,53 +6,79 @@ using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
-namespace Infrastructure.Repositories;
-public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+namespace Infrastructure.Repositories
 {
-    private readonly NotiAPIContext _context;
-    public GenericRepository(NotiAPIContext context)
+    public class GenericRepository<T> : IGenericRepository<T>
+        where T : BaseEntity
     {
-        _context = context;
-    }
+        private readonly NotiAPIContext _context;
 
-    public void Add(T entity)
-    {
-        throw new NotImplementedException();
-    }
+        public GenericRepository(NotiAPIContext context)
+        {
+            _context = context;
+        }
 
-    public void AddRange(IEnumerable<T> entities)
-    {
-        throw new NotImplementedException();
-    }
+        public virtual void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
 
-    public Task<IEnumerable<T>> Find(Expression<Func<T, bool>> expression)
-    {
-        throw new NotImplementedException();
-    }
+        public virtual void AddRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().AddRange(entities);
+        }
 
-    public Task<IEnumerable<T>> GetAllAsync()
-    {
-        throw new NotImplementedException();
-    }
+        public virtual IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression);
+        }
 
-    public Task<T> GetByIdAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+            //return (IEnumerable<T>)await _context.Paises.FromSqlRaw("SELECT * FROM pais").ToListAsync();
+        }
 
-    public void Remove(T entity)
-    {
-        throw new NotImplementedException();
-    }
+        public virtual async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
 
-    public void RemoveRange(IEnumerable<T> entity)
-    {
-        throw new NotImplementedException();
-    }
+        public virtual async Task<T> GetByIdAsync(string id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+        public virtual void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+        public virtual void RemoveRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
+        public virtual void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+        public virtual async Task<(int totalRegistros, IEnumerable<T> registros)> GetAllAsync(
+            int pageIndex,
+            int pageSize,
+            string _search)
+        {
+            var totalRegistros = await _context.Set<T>().CountAsync();
+            var registros = await _context
+                .Set<T>()
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+            return (totalRegistros, registros);
+        }
 
-    public void Update(T entity)
-    {
-        throw new NotImplementedException();
+        Task<IEnumerable<T>> IGenericRepository<T>.Find(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
